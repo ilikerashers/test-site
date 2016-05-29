af2e597d2cbef2e72c729dd63a9cc9b5ee315a8c
Jim Collins <jim.collins@blackdoglabs.co>
2016-05-29 21:12:27 +0100

    Initial commit

---
+++ helloworld.js
@@ -0,0 +1,8 @@
+console.log('hello world');
+
+//var a = [‘batman’, ‘robin’];
+//a.push(‘superman’);
+//console.log(a);
+
+//var employee = {name: 'jane developer', department: 'IT' };
+//console.log(employee);
